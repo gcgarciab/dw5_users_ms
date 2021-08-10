99ab6292a27e98060b1c516b27e2307d7ca35338
Camilo García <gcgarciab@unal.edu.co>
2021-08-09 19:20:58 -0500

    Verify model implemented

--- usersApp/admin.py
+++ usersApp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import BankUser
 
 # Register your models here.
+admin.site.register(BankUser)
--- usersProject/settings.py
+++ usersProject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,6 +42,18 @@ INSTALLED_APPS = [
     'usersApp',
 ]
 
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5), 
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1), 
+    'ROTATE_REFRESH_TOKENS': False, 
+    'BLACKLIST_AFTER_ROTATION': True, 
+    'UPDATE_LAST_LOGIN': False,
+    'ALGORITHM': 'HS256', 
+    'USER_ID_FIELD': 'id', 
+    'USER_ID_CLAIM': 'user_id',
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -50,6 +63,17 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = { 
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': ( 
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+AUTH_USER_MODEL = 'usersApp.BankUser'
 
 ROOT_URLCONF = 'usersProject.urls'
 
